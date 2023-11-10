992346df2c5343baed3971db6937e18c4b0208fc
kayofabio <kayofabio.dev@gmail.com>
2023-11-10 00:41:27 -0300

    menos repeticao de codigo

--- public/scripts/scripts.cjs
+++ public/scripts/scripts.cjs
@@ -3,41 +3,23 @@ function calcular(event) {
   const peso = Number(document.getElementById('peso').value);
   const idade = Number(document.getElementById('idade').value);
   const altura = Number(document.getElementById('altura').value);
-  const genero = seleciona('genero');
+  const genero = selecionaGeneroDaPessoa('genero');
   const dados = calcTMB(peso, idade, altura, genero);
-  const dadosIMC= calcIMC(peso, altura)
+  const dadosIMC= calcIMC(peso, altura);
 
-  document.getElementById('metabolismo_basal').innerHTML = Math.ceil(
-    dados.basal
-  );
-  document.getElementById('sedentario').innerHTML = Math.ceil(dados.sedentario);
-  document.getElementById('exercicio_leve').innerHTML = Math.ceil(
-    dados.exercicio_leve
-  );
-  document.getElementById('exercicio_moderado').innerHTML = Math.ceil(
-    dados.moderado
-  );
-  document.getElementById('ativo').innerHTML = Math.ceil(dados.ativo);
-  document.getElementById('super_ativo').innerHTML = Math.ceil(
-    dados.superAtivo
-  );
-  document.getElementById('ganhar_peso').innerHTML = Math.ceil(
-    dados.ganharPeso
-  );
-  document.getElementById('perder_peso').innerHTML = Math.ceil(
-    dados.perderPeso
-  );
+  showResults(dados, dadosIMC);
+};
 
-  document.getElementById('imc').innerHTML = Math.ceil(
-    dadosIMC
-  );
-  document.getElementById('imc_classification').innerHTML = calcTabelaIMC(dadosIMC) ;
-  
+function showResults(dados, dadosIMC) {
+  const listResult = document.querySelectorAll('.result-item');
+  listResult.forEach((item, index) => item.innerHTML = Math.ceil(dados[index]));
+
+  document.getElementById('imc').innerHTML = Math.ceil(dadosIMC);
+  document.getElementById('imc_classification').innerHTML = calcTabelaIMC(dadosIMC);
   document.getElementById('result-data').style.visibility = 'visible';
 }
 
-//pega o genero se masculino ou feminino
-function seleciona(id) {
+function selecionaGeneroDaPessoa(id) {
   const select = document.getElementById(id);
   return select.options[select.selectedIndex].value;
 }
@@ -49,18 +31,17 @@ function calcTMB(peso, idade, altura, genero) {
     genero === 'Masculino'
       ? 10 * peso + 6.25 * altura - 5 * idade + 5
       : 10 * peso + 6.25 * altura - 5 * idade - 161;
-  const resData = {
-    basal: res,
-    sedentario: 1.2 * res,
-    exercicio_leve: 1.375 * res,
-    moderado: 1.55 * res,
-    ativo: 1.725 * res,
-    superAtivo: 1.9 * res,
-    ganharPeso: res + 450,
-    perderPeso: res - 450,
-  };
+  const basal = res;
+  const sedentario = 1.2 * res;
+  const exercicioLeve = 1.375 * res;
+  const moderado = 1.55 * res;
+  const ativo = 1.725 * res;
+  const superAtivo = 1.9 * res;
+  const ganharPeso = res + 450;
+  const perderPeso = res - 450;
+  const resData = [basal, sedentario, exercicioLeve, moderado, ativo, superAtivo, ganharPeso, perderPeso];
   return resData;
-}
+};
 
 function calcIMC(peso, altura) {
   if (peso <= 0 || altura <= 0) {
@@ -71,6 +52,6 @@ function calcIMC(peso, altura) {
   const imc = peso / (alturaMetros * alturaMetros);
 
   return imc.toFixed(2);
-}
+};
 
 module.exports = { calcIMC,calcTMB }
